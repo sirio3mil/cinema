d531bf03c2e37fcb91190bfa5563e06fb740173f
Reynier de la Rosa <reynier.delarosa@digimobil.es>
2018-04-20 12:51:20 +0200

    updating naming convention

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 
     protected $table = 'User';
 
-    protected $primaryKey = 'userID';
+    protected $primaryKey = 'userId';
 
     const CREATED_AT = 'createdAt';
 
