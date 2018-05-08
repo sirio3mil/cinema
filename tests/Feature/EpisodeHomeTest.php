c5b19acfbd725a580116d824771ce68a511c642b
Reynier de la Rosa <sirio3mil@gmail.com>
2018-05-08 23:29:28 +0200

    new tests

---
+++ tests/Feature/EpisodeHomeTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sirio
+ * Date: 08/05/2018
+ * Time: 23:28
+ */
+
+namespace Tests\Feature;
+
+use App\Libraries\Scrapers\Imdb\Pages\Home;
+use PHPUnit\Framework\TestCase;
+
+class EpisodeHomeTest extends TestCase
+{
+
+    public function testGetTitle()
+    {
+
+    }
+
+    public function testGetEpisodeNumber()
+    {
+
+    }
+
+    public function testIsEpisode()
+    {
+
+    }
+
+    public function testGetSeasonNumber()
+    {
+
+    }
+
+    public function testGetTvShow()
+    {
+
+    }
+}
